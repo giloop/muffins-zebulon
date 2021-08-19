f347e2765092f2a321135765035351ea3b3c8bc2
giloop <giloop.theloop@gmail.com>
2021-08-19 16:25:52 +0200

    - ajout contenu des morceaux  - ajout de l'audio morceaux manquants  - ajout scrollbar intérieure aux slides  - photos description album

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ var tracks = [
   {"artist":"Muffin's Zebulon", "title": "I'll be here", "file":"4 i'll be here.mp3"},
   {"artist":"Muffin's Zebulon", "title": "One more year", "file":"5 one more year.mp3"},
   {"artist":"Muffin's Zebulon", "title": "It's time for you", "file":"6 it's time for you.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Badjock", "file":"9 badjock.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Virus", "file":"7 virus.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Alien", "file":"8 alien.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Bad Joke", "file":"9 badjock.mp3"},
   {"artist":"Muffin's Zebulon", "title": "Mystery girl", "file":"10 mystery girl.mp3"},
   {"artist":"Muffin's Zebulon", "title": "Very good mum", "file":"11 very good mum.mp3"}
 ];
@@ -16,27 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
   audio.addEventListener('ended', function(event) {
     tracksAudioNext();
   });
-
-  // // Ajout des morceaux depuis le fichier liste-morceaux.txt
-  // arTracks = [];
-  // fetch("./liste-morceaux.json")
-  // .then(response => {
-  //     if (!response.ok) {
-  //         throw new Error("HTTP error " + response.status);
-  //     }
-  //     return response.json();
-  // })
-  // .then(json => {
-  //     json.tracks.forEach(track => {
-  //     arTracks.push(track);
-  //     // Append node in page
-  //     // .add({
-  //     //    title:track.title,
-  //     //    artist:track.artist,
-  //     //    mp3:"audio/"+track.file
-  //     // });
-  //   });
-  // });
 });
 
 function tracksAudioSet(trackIndex) {
@@ -50,10 +31,12 @@ var track = this.tracks[trackIndex];
 
   this.trackCurrentIndex = trackIndex;
 }
+
 function tracksAudioNext() {
   this.trackCurrentIndex = ++this.trackCurrentIndex % this.tracks.length;
   tracksAudioSet(this.trackCurrentIndex);
 }
+
 function tracksAudioPrev() {
   if (this.trackCurrentIndex === 0) {
     this.trackCurrentIndex = this.tracks.length - 1;
