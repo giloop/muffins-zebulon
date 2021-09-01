a31fe33726b6419e999d23b1ebb3630f03b20726
Denis Braud <denisbraud@gmail.com>
2021-09-01 09:42:29 +0200

    img + mp3 : renommage + suppr. gros fichiers
    + versions mp3 gérées par Nico
    + allégement repo git :
    see https://docs.github.com/en/github/managing-large-files/working-with-large-files

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var trackCurrentIndex = 0;
 var tracks = [
-  {"artist":"Muffin's Zebulon", "title": "Time's goin by", "file":"1 time's goin by baby's comin.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Girl and boy", "file":"2 girl and boy.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Vinaigre", "file":"3 vinaigre.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "I'll be here", "file":"4 i'll be here.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "One more year", "file":"5 one more year.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "It's time for you", "file":"6 it's time for you.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Virus", "file":"7 virus.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Alien", "file":"8 alien.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Bad joke", "file":"9 badjock.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "Mystery girl", "file":"10 mystery girl.mp3"},
-  {"artist":"Muffin's Zebulon", "title": "A very good mum", "file":"11 very good mum.mp3"}
+  {"artist":"Muffin's Zebulon", "title": "Time's goin by",    "file":"MuffinsZebulon-2021-TimesRoll-01-TimesGoinBy.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Girl and boy",      "file":"MuffinsZebulon-2021-TimesRoll-02-GirlAndBoy.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Vinaigre",          "file":"MuffinsZebulon-2021-TimesRoll-03-Vinaigre.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "I'll be here",      "file":"MuffinsZebulon-2021-TimesRoll-04-IllBeHere.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "One more year",     "file":"MuffinsZebulon-2021-TimesRoll-05-OneMoreYear.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "It's time for you", "file":"MuffinsZebulon-2021-TimesRoll-06-ItsTimeForYou.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Virus",             "file":"MuffinsZebulon-2021-TimesRoll-07-Virus.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Alien",             "file":"MuffinsZebulon-2021-TimesRoll-08-Alien.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Bad joke",          "file":"MuffinsZebulon-2021-TimesRoll-09-BadJoke.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "Mystery girl",      "file":"MuffinsZebulon-2021-TimesRoll-10-MysteryGirl.mp3"},
+  {"artist":"Muffin's Zebulon", "title": "A very good mum",   "file":"MuffinsZebulon-2021-TimesRoll-11-AVeryGoodMum.mp3"}
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -25,6 +25,9 @@ var track = this.tracks[trackIndex];
   var audio = document.getElementById('audio');
   audio.src = "audio/"+track.file;
   audio.play();
+
+  var audio = document.getElementById('audio-alt');
+  audio.href = "audio/"+track.file;
 
   var audioTitle = document.getElementById('audio-title');
   audioTitle.textContent = track.title;
